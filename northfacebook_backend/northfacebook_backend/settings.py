f2413a45522cee96de36e31a71fd8cecd859fd2e
Newspring97 <qotoqha970@hanmail.net>
2017-04-28 00:56:19 +0900

    settings.py에 rest_framework, homepage, corsheaders추가 및 cors관련 기본 세팅 처리

--- northfacebook_backend/northfacebook_backend/settings.py
+++ northfacebook_backend/northfacebook_backend/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'homepage',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'northfacebook_backend.urls'
@@ -118,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# CORS ALLOWANCE
+CORS_ORIGIN_ALLOW_ALL = True
