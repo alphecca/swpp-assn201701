3680566611a2c6e386481696882577eb502c2487
Newspring <qotoqha970@hanmail.net>
2017-05-04 16:33:15 +0900

    working version of init.sh - port 8000

--- progress/init.sh
+++ progress/init.sh
@@ -1,8 +1,4 @@
 tmux \
-    new-session -s new-session -n backend "python3 ../northfacebook_backend/manage.py runserver 0.0.0.0:7777" \; \
+    new-session -s new-session -n backend "python3 ../northfacebook_backend/manage.py runserver 0.0.0.0:8000" \; \
     new-window -n frontend "cd ../northfacebook_frontend; npm start" \; \
-    new-window -n testWindow \; \
-    detach-client
-sleep 5
-./test.sh
-tmux kill-session -t new-session
+    new-window -n testWindow "./test.sh; sleep 3; tmux kill-session -t new-session"
