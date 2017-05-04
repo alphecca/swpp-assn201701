02a40f746f60c848c8558deac5f158303410c11c
Newspring <qotoqha970@hanmail.net>
2017-05-04 18:07:03 +0900

    test port edit

--- progress/test.sh
+++ progress/test.sh
@@ -1,3 +1,3 @@
-python3 backend_tests.py wlxyzlw.iptime.org:7777/
+python3 backend_tests.py wlxyzlw.iptime.org:8000/
 #python3 inittest.py wlxyzlw.iptime.org:8000/
 #python3 signUpChecker.py wlxyzlw.iptime.org:3000/
