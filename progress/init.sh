3b9c2624741428ed8a372e7e7b1bc10860c47026
Newspring <qotoqha970@hanmail.net>
2017-05-05 02:30:27 +0900

    add sleep for checking the result

--- progress/init.sh
+++ progress/init.sh
@@ -1,4 +1,4 @@
 tmux \
     new-session -s new-session -n backend "python3 ../northfacebook_backend/manage.py runserver 0.0.0.0:8000" \; \
     new-window -n frontend "cd ../northfacebook_frontend; npm start" \; \
-    new-window -n testWindow "./test.sh"
+    new-window -n testWindow "./test.sh; sleep 3"
