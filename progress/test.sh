02114e6b16822f306bb502cdc637c43fb30bf02f
An Dantae <48adt@naver.com>
2017-05-05 22:44:32 +0900

    pipelines do backend test automatically

--- progress/test.sh
+++ progress/test.sh
@@ -1,6 +1,3 @@
-# TODO Use tmux to run server and tests simultaneously
-tmux new -s server
-python3 ../northfacebook_backend/manage.py runserver
 python3 backend_tests.py http://127.0.0.1:8000/
 #python3 inittest.py http://127.0.0.1:8000/
 #python3 signUpChecker.py http://127.0.0.1:3000/
