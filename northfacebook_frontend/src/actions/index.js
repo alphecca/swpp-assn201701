b140fcdc6c2a3032f66568abe9dfaa3149ec7512
Eu Ka On <wlxyzlw@bacchus.snucse.org>
2017-05-05 12:28:47 +0900

    ok

---
+++ northfacebook_frontend/src/actions/index.js
@@ -0,0 +1,34 @@
+// When the user inputs username, password and clicks the 'Sign In' button, this action is invoked and Saga requests GET to 'auth' in backend page.
+export const signIn = (username, password) => {
+    return {
+        type: 'SIGN_IN',
+        username,
+        password
+    }
+}
+
+export const SIGN_IN = 'SIGN_IN'
+
+// When the user is authenticated and succeeds to sign in, this action is invoked by Saga and reducer stores the user's authorization to its state.
+export const authenticate = (auth) => {
+    return {
+        type: 'AUTHENTICATE',
+        auth
+    }
+}
+
+// When the user enters username, password and pwdverification and clicks the '회원가입', this action is invocked and Saga requests POST to 'User List' in backend page.
+export const postSignUp = (username, password) => {
+    return {
+        type: 'POST_SIGN_UP',
+        username,
+        password
+    }
+}
+//When the user clicks the 'Sign Out' button, this action is invoked
+export const signOut = () => {
+    return {
+       type: 'SIGN_OUT',
+    }
+}
+export const SIGN_OUT = 'SIGN_OUT'
---
+++ progress/inittest.py
@@ -0,0 +1,40 @@
+import json
+import requests
+from time import sleep
+from random import randint
+
+def create_users(N):
+    ls = []
+    for i in range(1, N):
+        ls.append(("test{0}".format(i), "test{0}passwd".format(i)))
+    return ls
+
+def post_or_error_anon(link, data):
+    sleep(0.05)
+    try:
+        res = requests.post(link, data=data)
+        if res.status_code != 201:
+            print("ERROR: Cannot post {0} : {1}".format(link, res.status_code))
+            exit(1)
+    except Exception:
+        print("ERROR: Cannot post {0}".format(link))
+        exit(1)
+
+userN = 10
+user_pairs = create_users(userN)
+
+link = "http://wlxyzlw.iptime.org:8000/users/" # TODO If you want to change port of url, revise this.
+print("1. Creating new users.")
+try:
+    for i in range(1,userN+1):
+        username = "test{0}".format(i)
+        pwd = "test{0}passwd".format(i)
+        res = requests.delete(link, auth=(username, pwd))
+except Exception:
+    pass
+
+for i in range(1,userN):
+    body = {"username": "test{0}".format(i).encode("ascii"), "password": "test{0}passwd".format(i).encode("ascii")}
+    post_or_error_anon(link, body)
+
+print("INITIALIZE SUCCESSFUL")
