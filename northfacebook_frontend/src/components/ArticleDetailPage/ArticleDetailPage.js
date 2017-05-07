0e76eaaed654c6416f04c84be89b3bd334390dd6
Newspring <qotoqha970@hanmail.net>
2017-05-07 12:02:56 +0900

    let's change proptypes 예이 대환장파티

--- northfacebook_frontend/src/components/ArticleDetailPage/ArticleDetailPage.js
+++ northfacebook_frontend/src/components/ArticleDetailPage/ArticleDetailPage.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Article from '../TimeLinePage/Article.js'
+import {connect} from 'react-redux'
 
 class ArticleDetailPage extends React.Component {
     render() {
@@ -10,5 +12,26 @@ class ArticleDetailPage extends React.Component {
                )
     }
 }
+/*
+let mapStateToProps = (state) => {
+     return {
+         article: {
+             article: Object.assign(state.parent_article),
+             id: Object.assign(state.parent_article.id),
+             writerId: "a"+state.parent_article.id+"_writer_field",
+             username: state.parent_article.owner,
+             textId: "a"+state.parent_article.id+"_text_field",
+             articleText: state.parent_article.text,
+             likeNum: state.parent_article.like_num,
+             likeButtonId: "a"+state.parent_article.id+"_like_button_field",
+             editButtonId: "a"+state.parent_article.id+"_edit_button_field",
+             deleteButtonId: "a"+state.parent_article.id+"_delete_button_field",
+             detailButtonId: "a"+state.parent_article.id+"_detail_button_field",
+             replyNum: state.parent_article.children_num
+         }
+     }
+}
 
+ArticleDetailPage = connect(mapStateToProps)(ArticleDetailPage)
+*/
 export default ArticleDetailPage
