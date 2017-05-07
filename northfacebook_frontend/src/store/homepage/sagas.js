67bf5b03d3d955b81096e883017a288782837678
Newspring <qotoqha970@hanmail.net>
2017-05-07 15:55:57 +0900

    full of error but working

--- northfacebook_frontend/src/store/homepage/sagas.js
+++ northfacebook_frontend/src/store/homepage/sagas.js
@@ -8,8 +8,8 @@ import {createBrowserHistory} from 'history'
 var xhr = require('xhr-promise-redux');
 
 //TODO change before send pull request
-const fixed_url = "http://wlxyzlw.iptime.org:7777/";
-const auth_check_url = fixed_url+'auth/';
+const fixed_url = "http://wlxyzlw.iptime.org:7777";
+const auth_check_url = fixed_url+'/auth/';
 //const article_get_url = fixed_url+'mainpage/';
 
 const history = createBrowserHistory();
@@ -20,26 +20,30 @@ const routes = {
         yield spawn(watchSignIn)
     },
     '/main': function *timeLinePageSaga() {
-        yield spawn(updateState, 'mainpage/')
+        yield spawn(updateState, '/mainpage/')
         yield spawn(watchSignOut)
         yield spawn(watchWrite)
         yield spawn(watchLike)
         yield spawn(watchDetail)
     },
-    '/article/:id?': function *articleDetailPageSaga() {
-        yield spawn(updateDetailPage)
+    '/article/:id': function *articleDetailPageSaga() {
+        yield call(updateDetailPage)
+        yield spawn(watchSignOut)
+        yield spawn(watchLike)
+        yield spawn(watchWrite)
     },
     '/sign_up': function *signUpPageSaga() {
         yield spawn(watchSignUp)
     },
-    '/write': function *writeArticleSaga(){
-        yield spawn(updateState, 'mainpage/')
+    '/write/:id?': function *writeArticleSaga(){
+        yield spawn(updateWritePage)
         yield spawn(watchSignOut)
+        yield spawn(watchPost)
     }
 }
 
 function* getNewState(state, path) {
-    console.log(path)
+//    console.log(path)
     let data;
     try {
         console.log("hoeee")
@@ -102,8 +106,8 @@ export function* sign_in(data) {
         alert("Succeed to sign in! :)")
         yield put(actions.authenticate(encodedData))
         const state = yield select()
-        history.push('/main', state)
-        yield put(actions.changeUrl('/main'))
+        const newState = yield call(getNewState, state, '/mainpage/')
+        history.push('/main', newState)
     }
     catch(error) {
         console.log(error)
@@ -112,9 +116,9 @@ export function* sign_in(data) {
             console.log("Succeed to sign in!")
             alert("Succeed to sign in! :)")
             yield put(actions.authenticate(encodedData));
-            const newState = yield select()
+            const state = yield select()
+            const newState = yield call(getNewState, state, '/mainpage/')
             history.push('/main', newState)
-            yield put(actions.changeUrl('/main'));
         }
         else if (error.statusCode === 0) {
             console.log("Backend server is not available!")
@@ -148,7 +152,7 @@ export function *signUp(data) {
     yield call(console.log, data.username + " " + data.password);
     const auth = window.btoa(data.username+":"+data.password);
     try {
-        yield call(xhr.post, fixed_url+'users/', {
+        yield call(xhr.post, fixed_url+'/users/', {
             headers: {
               'Content-Type': 'application/json',
               Accept: 'application/json'
@@ -160,19 +164,20 @@ export function *signUp(data) {
         alert("Succeed to sign up!");
         yield put(actions.authenticate(auth))
         const state = yield select()
-        history.push('/main', state)
-        yield put(actions.changeUrl('/main'))
+        const newState = yield call(getNewState, state, '/mainpage/')
+        alert(JSON.stringify(newState))
+        history.push('/main', newState)
     }
     catch(error) {
         console.log(error)
         console.log(error.statusCode)
         if(error.statusCode === 201) {
-            console.log("Succeed to sign up without exception!");
+            console.log("Succeed to sign up without exception!2");
             alert("Succeed to sign up!");
             yield put(actions.authenticate(auth));
             const state = yield select()
-            history.push('/main', state)
-            yield put(actions.changeUrl('/main'))
+            const newState = yield call(getNewState, state, '/mainpage/')
+            history.push('/main', newState)
         }
         else if(error.statusCode === 405) { //Temporary status code for duplicated username
             console.log("User already exist!");
@@ -186,11 +191,13 @@ export function *signUp(data) {
             alert("Fail to sign up! Server is not available!");
         }
         else if(Object.keys(error).length === 0) {
-            console.log("Succeed to sign up without exception!");
+            console.log("Succeed to sign up without exception!3");
             alert("Succeed to sign up!");
+            yield put(actions.authenticate(auth))
             const state = yield select()
-            history.push('/main', state)
-            yield put(actions.changeUrl('/main'))
+            const newState = yield call(getNewState, state, '/mainpage/')
+            history.push('/main', newState)
+
         }
         else {
             console.log("버그 잡아라 뉴스프링 깔깔깔");
@@ -221,11 +228,24 @@ export function* sign_out(){
 export function* watchWrite(){
     while(true) {
         console.log("댓글을 써라 노딩코예야")
-        yield take('WRITE_ARTICLE')
+        const data = yield take('WRITE_ARTICLE')
         console.log("watch reply")
         const state = yield select()
-        history.push('/write', state)
-        yield put(actions.changeUrl('/write'))
+//        alert(JSON.stringify(data))
+        if(data.id === null) {
+//            alert(JSON.stringify(state))
+            history.push('/write', state)
+        }
+        else {
+            const newState = yield call(getNewState, state, '/article/'+data.id.id+'/article/')
+//            alert(data.id.id+" "+JSON.stringify(newState))
+            history.push('/write/'+data.id.id, Object.assign({}, newState, {
+                authorization: newState.authorization,
+                articles: newState.articles,
+                parent_article: data.id
+            }))
+        }
+//        yield put(actions.changeUrl('/write'))
     }
 }
 
@@ -234,14 +254,22 @@ export function* watchWrite(){
 //TODO change HARD CODING into more beautiful code
 //TODO remove statusCode 0 error
 function *updateState(path) {
-    console.log(history.location.state);
     if(history.location.state === undefined || history.location.state.authorization === "")
         yield put(actions.changeUrl('/'))
     else {
         const newState = yield call(getNewState, history.location.state, path)
+//        console.log(newState)
+//        console.log(history.location.state)
         if(newState !== null) {
             yield put(actions.setState(newState))
-            return
+            const tmp = yield select()
+            console.log(tmp)
+            console.log("웅앵웅 초키포키")
+            if(JSON.stringify(tmp) !== JSON.stringify(history.location.state)) {
+                history.push(history.location.pathname, newState)
+                console.log('NUGABA')
+//                window.location.reload()
+            }
         }
     }
 }
@@ -252,7 +280,7 @@ function *watchLike() {
         const data = yield take('POST_LIKE')
         console.log(data)
         try {
-            yield call(xhr.post, fixed_url+'article/'+data.id+'/like/', {
+            yield call(xhr.post, fixed_url+'/article/'+data.id+'/like/', {
             headers: {
               'Authorization': 'Basic '+ window.btoa(data.auth),
               'Content-Type': 'application/json',
@@ -262,7 +290,25 @@ function *watchLike() {
         });
         }
         catch(error) {
-            console.log(error)
+        if(error.statusCode === 201) {
+            console.log("Succeed2");
+//            const state = yield select()
+//            history.push('/main', state)
+//            console.log(history.location.pathname)
+        }
+        else if(error.statusCode === 404) {
+            console.log("Backend server does not exist!");
+        }
+        else if(error.statusCode === 0) {
+            console.log("Backend server is not available!");
+        }
+        else if(Object.keys(error).length === 0) {
+            console.log("Succeed3");
+//            const state = yield select()
+//            history.push('/main', state)
+//            console.log(history.location.pathname)
+        }
+
         }
     }
 }
@@ -273,13 +319,9 @@ function *watchDetail() {
         const data = yield take('ARTICLE_DETAIL')
         alert('detail button clicked')
         const state = yield select()
-        alert(JSON.stringify(data))
-        const path = 'article/'+data.id
-        const newState = yield call(getNewState, state, path)
-//        alert(JSON.stringify(newState))
+        const path = '/article/'+data.id.id
+        const newState = yield call(getNewState, state, path+'/article/')
         history.push(path, newState)
-        alert(JSON.stringify(history.location))
-        yield put(actions.changeUrl(data.id))
     }
 }
 
@@ -287,12 +329,67 @@ function *watchDetail() {
 /////ARTICLE DETAIL PAGE/////
 function *updateDetailPage() {
     console.log("welcome to article detail page")
-    console.log(history.location)
-    let data = yield select()
-//    alert(JSON.stringify(history.location.state))
-    yield call(updateState, 'article/'+data.parent_article)
-    data = yield select()
-    alert(JSON.stringify(data))
+//    console.log(history.location)
+    yield call(updateState, history.location.pathname+'/article/')
+    console.log(yield select())
+}
+
+/////WRITE PAGE/////
+function *updateWritePage() {
+    console.log("welcome to write page")
+    let url = '/article/'
+    if(history.location.pathname === '/write') {
+//        alert(JSON.stringify(history.location))
+        yield call(updateState, '/mainpage/')
+    }
+    else {
+        url = url+history.location.pathname.split("/")[2]+'/article/'
+        yield call(updateState, url)
+    }
+    console.log(yield select())
+
+}
+
+function *watchPost() {
+    const data = yield take('ADD_ARTICLE')
+//    alert(JSON.stringify(data))
+    const url = data.id === null? '/article/': '/article/'+data.id.id + '/article/'
+//    alert(url)
+    try {
+        yield call(xhr.post, fixed_url + url, {
+            headers: {
+              'Authorization': 'Basic '+ window.btoa(history.location.state.authorization),
+              'Content-Type': 'application/json',
+               Accept: 'application/json'
+            },
+            responseType: 'json',
+            body: JSON.stringify({"text": data.text})//'{"text":'+data.text+'}'
+        })
+        const state = yield select()
+        history.push('/main', state)
+        console.log(history.location.pathname)
+    }
+    catch(error) {
+        if(error.statusCode === 201) {
+            console.log("Succeed2");
+            const state = yield select()
+            history.push('/main', state)
+            console.log(history.location.pathname)
+        }
+        else if(error.statusCode === 404) {
+            console.log("Backend server does not exist!");
+        }
+        else if(error.statusCode === 0) {
+            console.log("Backend server is not available!");
+        }
+        else if(Object.keys(error).length === 0) {
+            console.log("Succeed3");
+            const state = yield select()
+            history.push('/main', state)
+            console.log(history.location.pathname)
+        }
+
+    }
 }
 
 export default function* saga() {
