e1bffbfb97f13d1e78a9e7a52b7d64e8d7da9cbe
An Dantae <48adt@naver.com>
2017-05-07 22:25:58 +0900

    이번엔 되겠지

--- progress/init.sh
+++ progress/init.sh
@@ -1,4 +1,4 @@
 tmux \
     new-session -s new-session -n backend "python3 ../northfacebook_backend/manage.py runserver 0.0.0.0:8000" \; \
     new-window -n frontend "cd ../northfacebook_frontend; npm start" \; \
-    new-window -n testWindow "echo 'Wait for running server...'; sleep 5; ./test.sh; sleep 3; tmux kill-session -t new-session"
+    new-window -n testWindow "echo 'Wait 5 seconds for running server...'; sleep 5; ./test.sh; sleep 3; tmux kill-session -t new-session"
