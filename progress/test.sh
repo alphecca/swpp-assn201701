dc9a9d84a552a9f5335f57cb5519d6a64cf2925e
Newspring97 <qotoqha970@hanmail.net>
2017-05-14 16:00:58 +0900

    detail test제외 전체적으로 코딩 완료

--- progress/test.sh
+++ progress/test.sh
@@ -1,3 +1,4 @@
 python3 backend_tests.py http://127.0.0.1:8000/
+#python3 myTest.py http://127.0.0.1:8000/ http://127.0.0.1:3000/
 #python3 inittest.py http://127.0.0.1:8000/
 #python3 signUpChecker.py http://127.0.0.1:3000/
