2c4af23615f375fb1814bdb96c7f26504f0f058e
An Dantae <48adt@naver.com>
2017-06-05 23:05:46 +0900

    accept Newspring's opinion

--- northfacebook_frontend/src/components/WallPage/WallPage.js
+++ northfacebook_frontend/src/components/WallPage/WallPage.js
@@ -10,7 +10,6 @@ class WallPage extends React.Component {
                     <SignOut />
                     <div className="TimeLine">
                         <span id="wall_info">{this.props.profile_user}의 담벼락</span>
-//                        <button id="back_to_profile" onClick={this.props.onBackToProfile}>Back to Profile</button>
                         <WallArticleList />
                     </div>
                 </div>
@@ -23,14 +22,6 @@ let mapStateToProps = (state) => {
         profile_user: state.profile_user !== null ? Object.assign(state.profile_user.username) : null //TODO 이후 프로필 전체 구현 끝나면 수정 예정
     }
 }
-/*
-let mapDispatchToProps = (dispatch) => {
-    return {
-        onBackToProfile: () => dispatch({type: 'TO_PROFILE'})
-    }
-}
-*/
-//WallPage = connect(mapStateToProps, mapDispatchToProps)(WallPage);
 WallPage = connect(mapStateToProps)(WallPage);
 
 export default WallPage;
