6f7b82f958d1a24d15ffde7d801e97865cf3a9db
Eu Ka On <wlxyzlw@bacchus.snucse.org>
2017-06-10 00:35:44 +0900

    more

--- northfacebook_frontend/src/components/WallPage/WallArticleList.js
+++ northfacebook_frontend/src/components/WallPage/WallArticleList.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import WallArticle from './WallArticle.js'
+import {moreArticle} from '../../actions'
 import {connect} from 'react-redux'
 
+var re=0;
 class WallArticleList extends React.Component {
     render() {
-        const list = this.props.articles
+         const Scroll = () => {
+          this.props.onMoreClick()
+        };
+        window.onscroll = function(ev){
+          if(re<document.body.scrollHeight && (window.innerHeight + window.scrollY)>=document.body.scrollHeight){
+            re=document.body.scrollHeight;
+            Scroll();
+            alert("더!");
+          }
+        };
+       const list = this.props.articles
         return (
                 <div id="article_list_field" className="ArticleList">
                 {list.map(article => <WallArticle key={article.id} {...article}/>)}
@@ -15,7 +27,7 @@ class WallArticleList extends React.Component {
 
 let mapStateToProps = (state) => {
     return {
-        articles: Object.assign(state.articles).slice(0, 5).map(article => JSON.parse(JSON.stringify(
+        articles: Object.assign(state.articles).slice(0, state.load+5).map(article => JSON.parse(JSON.stringify(
                           {
                               article: article,
                               id: article.id
@@ -25,6 +37,10 @@ let mapStateToProps = (state) => {
     }
 }
 
-WallArticleList = connect(mapStateToProps)(WallArticleList)
+WallArticleList = connect(mapStateToProps,(dispatch)=>{
+  return{
+    onMoreClick:()=>dispatch(moreArticle())
+  }
+})(WallArticleList)
 
 export default WallArticleList
