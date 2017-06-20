9a1c89d9594716f023a31d7916c87e142a49679b
An Dantae <48adt@naver.com>
2017-06-20 12:48:36 +0900

    adjust frontend_chatting_tests.py

--- progress/frontend_.py
+++ progress/frontend_.py
@@ -457,6 +457,7 @@ def chatRoomVerification(driver, link, uname, upwd):
     sleep(delayTime)
     alert(driver, "제대로 입력하시오.")
     driver.find_element_by_id('cancel_button_field').click()
+    sleep(delayTime)
     print("2-3. make three new room")
     for t in range(1,4):
         check(driver, "new_room_button_field")
@@ -502,11 +503,11 @@ def chatRoomVerification(driver, link, uname, upwd):
             print("ERROR: Cannot get {0}".format(link))
         roomId = "room"+str(res.json()[t-4]['id'])
 
-        check(driver, roomId+"_join_field")
+        check(driver, roomId+"_quit_field")
         check(driver, roomId+"_chat_field")
-        driver.find_element_by_id(roomId+"_join_field").click()
+        if t != 3:
+            driver.find_element_by_id(roomId+"_quit_field").click()
         sleep(delayTime)
-        alert(driver,"당신은 이미 함께하였소.")
     sleep(delayTime)
     driver.find_element_by_id(roomId+"_chat_field").click()
     sleep(delayTime)
@@ -581,7 +582,7 @@ def B_chatRoomVerification(driver, roomId):
     driver.find_element_by_id("input_text_field").send_keys("this text cannot be sent")
     driver.find_element_by_id('post_text_button_field').click()
     sleep(delayTime)
-    alert(driver, "함께하고 얘기하시오.")
+    alert(driver, "참여하고 얘기하시오.")
     sleep(delayTime*2)
     print("2-1. join the room")
     # at room list
